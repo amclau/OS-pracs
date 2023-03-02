fa53916ba13586ab02298bc3fadffe29957f0f95
amclau <98709499+amclau@users.noreply.github.com>
2023-03-02 05:16:35 +0000

    "CODE RUN SYNC"

---
+++ threadDeep.java
@@ -0,0 +1,39 @@
+/**
+ * threadmain
+ */
+class Booking extends Thread
+{   
+    int available=2;
+    int require;
+    Booking(int n)
+    {
+        require=n;
+    }
+
+    public void run()
+    {
+     System.out.println("Welcome to ticket booking system "+Thread.currentThread().getName()+"!!!!!!!!!!!!!!!!!!!!!!!!!");   
+    System.out.println("No of ticket left :"+available);
+        if(available>=require)
+        {
+            System.out.println("Ticket is booked");
+
+            available=available-require;
+        }
+        else if(available<=require){
+            System.out.println("Ticket not available");
+        }
+    }
+
+} 
+class threadDeep {
+public static void main(String[] args) {
+    Booking user1=new Booking(2);
+    Booking user2=new Booking(2);
+    Thread t1 = new Thread(user1);
+    Thread t2 = new Thread(user2);
+    
+    t1.start();
+    t2.start();
+}
+}
