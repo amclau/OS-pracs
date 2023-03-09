ac988c6cb602963f59be524d7473be9d9bfefaec
amclau <98709499+amclau@users.noreply.github.com>
2023-03-09 05:57:22 +0000

    "SJF"

--- SJFPrem.java
+++ SJFPrem.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
 
-class FCFS
+class SJFPrem
 {
- 
+
+    public static int findMin(int bt[]) 
+    {
+    int minP=0;    
+    }
     public static void main(String args[]) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("FCFS CODE :-\n");
+        System.out.println("SJF Prem :-\n");
 
         System.out.println("Enter no of process :");
         int process=sc.nextInt();
-        int At[]={0,1,2,3,6};
-        int Bt[]={2,6,4,9,12};
+        int At[]={0,1,2,3,4,5};
+        int Bt[]={7,5,3,1,2,1};
         int ct[]=new int[process];
         int Tat[]=new int[process];
         int wt[]=new int[process];
+        int Tbt=19;
         System.out.println("Enter arrival times & Burst times :\n");
         
         // for(int i=0;i<process;i++)
@@ -24,16 +29,22 @@ class FCFS
         // Bt[i]=sc.nextInt(); 
         // }
 
-    ct[0]=Bt[0];
-    for(int i=1;i<process;i++)
-    {
-        ct[i]=Bt[i]+ct[i-1];   
-    }
-    for(int i=0;i<process;i++)
+int current_time=0;
+for (int i = 0; i <=Tbt; i++) {
+    current_time++;
+    processIndex=findMin();
+
+}
+
+
+
+
+
+for(int i=0;i<process;i++)
     { 
         Tat[i]=ct[i]-At[i];    
         wt[i]=Tat[i]-Bt[i]; 
-}
+    }
     
 
 for(int i=0;i<process;i++)
